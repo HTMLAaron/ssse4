ff37bb5895ee29cc6efdcfa757b881393c0f7065
Aaron <43405823+HTMLAaron@users.noreply.github.com>
2019-12-01 14:45:15 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 const ObjectId = require('mongodb').ObjectID;
 const mongoDBurl = 'mongodb+srv://aaron:aaronso@aarondb-ep2mi.mongodb.net/test?retryWrites=true&w=majority';
-const dbName = 'tset';
+const dbName = 's381assignment';
 
 
 const server = http.createServer((req,res) => {
